6fd7e6d8083f1b6ec40647b43de463b456564806
whiteroma <roma.white95@gmail.com>
2022-11-23 21:33:31 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,11 +11,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-// basename="/goit-react-hw-08-phonebook/"
